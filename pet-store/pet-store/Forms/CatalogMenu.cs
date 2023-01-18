1698bdf296cbab7e7f700b2b7e10342fb7310c6a
Bogdan Petrovskiy <fzzksiniy@icloud.com>
2023-01-18 04:50:13 +0300

    Create catalog menu

---
+++ pet-store/pet-store/Forms/CatalogMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pet_store.Forms
+{
+    public partial class CatalogMenu : Form
+    {
+        public CatalogMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
